94a7156344a096f906af8a8d16573b61775f3968
Meskini Driss <driss.meskini@outlook.fr>
2025-10-15 15:34:15 +0200

    Position & Station Added

---
+++ Velo/Proxy/Station.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    internal class Station
+    {
+        private int number;
+
+        private string contract_name;
+
+        private string name;
+
+        private string address;
+
+        private Position position;
+
+        private bool banking;
+
+        private bool bonus;
+
+        private int bike_stands;
+
+        private int available_bike_stands;
+
+
+        private int available_bike;
+
+
+        private string status;
+
+        public Station()
+        {
+            position = new Position(1, 2);
+        }
+
+    }
+}
