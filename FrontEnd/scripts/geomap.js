16ee81923eb8ec0837601d8dcafb6c030232912c
Driss <driss.meskini@outlook.fr>
2025-11-06 18:52:58 +0100

    le chemin s'affiche btw

--- FrontEnd/scripts/geomap.js
+++ FrontEnd/scripts/geomap.js
@@ -1,5 +1,7 @@
+let map; // ← rendre la carte globale
+
 function loadMap() {
-    const map = L.map('map').setView([48.8566, 2.3522], 13);
+    map = L.map('map').setView([48.8566, 2.3522], 13);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '© OpenStreetMap Contributors'
@@ -11,7 +13,6 @@ async function findPath() {
         const departureComponent = document.getElementById("departure");
         const arrivalComponent = document.getElementById("arrival");
 
-        // Accéder aux inputs à l’intérieur du shadowRoot
         const departureInput = departureComponent.shadowRoot.querySelector("input");
         const arrivalInput = arrivalComponent.shadowRoot.querySelector("input");
 
@@ -21,62 +22,37 @@ async function findPath() {
         }
 
         const departure = encodeURIComponent(departureInput.value.trim());
-        console.log("Departure set :", departure);
         const arrival = encodeURIComponent(arrivalInput.value.trim());
-        console.log("Arrival set :", arrival);
 
         if (!departure || !arrival) {
             console.warn("Veuillez remplir les deux champs.");
             return;
         }
 
+        // utilisation de la réponse mockée
+        const response = JSON.parse('{\"pedestrianPath\":[{\"type\":\"FeatureCollection\",\"bbox\":[1.467649,43.584044,1.46773,43.584077],\"features\":[{\"bbox\":[1.467649,43.584044,1.46773,43.584077],\"type\":\"Feature\",\"properties\":{\"segments\":[{\"distance\":7.5,\"duration\":5.4,\"steps\":[{\"distance\":7.5,\"duration\":5.4,\"type\":11,\"instruction\":\"Head northwest on Rue Ferrando\",\"name\":\"Rue Ferrando\",\"way_points\":[0,1]},{\"distance\":0.0,\"duration\":0.0,\"type\":10,\"instruction\":\"Arrive at Rue Ferrando, on the right\",\"name\":\"-\",\"way_points\":[1,1]}]}],\"way_points\":[0,1],\"summary\":{\"distance\":7.5,\"duration\":5.4}},\"geometry\":{\"coordinates\":[[1.46773,43.584044],[1.467649,43.584077]],\"type\":\"LineString\"}}],\"metadata\":{\"attribution\":\"openrouteservice.org | OpenStreetMap contributors\",\"service\":\"routing\",\"timestamp\":1762429278478,\"query\":{\"coordinates\":[[1.4677301,43.5840438],[1.4677552,43.5842119]],\"profile\":\"foot-walking\",\"profileName\":\"foot-walking\",\"format\":\"geojson\"},\"engine\":{\"version\":\"9.3.0\",\"build_date\":\"2025-06-06T15:39:25Z\",\"graph_date\":\"2025-10-30T00:32:48Z\",\"osm_date\":\"2025-10-20T00:00:01Z\"}}},{\"type\":\"FeatureCollection\",\"bbox\":[1.467649,43.584044,1.46773,43.584077],\"features\":[{\"bbox\":[1.467649,43.584044,1.46773,43.584077],\"type\":\"Feature\",\"properties\":{\"segments\":[{\"distance\":7.5,\"duration\":5.4,\"steps\":[{\"distance\":7.5,\"duration\":5.4,\"type\":11,\"instruction\":\"Head northwest on Rue Ferrando\",\"name\":\"Rue Ferrando\",\"way_points\":[0,1]},{\"distance\":0.0,\"duration\":0.0,\"type\":10,\"instruction\":\"Arrive at Rue Ferrando, on the right\",\"name\":\"-\",\"way_points\":[1,1]}]}],\"way_points\":[0,1],\"summary\":{\"distance\":7.5,\"duration\":5.4}},\"geometry\":{\"coordinates\":[[1.46773,43.584044],[1.467649,43.584077]],\"type\":\"LineString\"}}],\"metadata\":{\"attribution\":\"openrouteservice.org | OpenStreetMap contributors\",\"service\":\"routing\",\"timestamp\":1762429278632,\"query\":{\"coordinates\":[[1.4677301,43.5840438],[1.4677552,43.5842119]],\"profile\":\"foot-walking\",\"profileName\":\"foot-walking\",\"format\":\"geojson\"},\"engine\":{\"version\":\"9.3.0\",\"build_date\":\"2025-06-06T15:39:25Z\",\"graph_date\":\"2025-10-30T00:32:48Z\",\"osm_date\":\"2025-10-20T00:00:01Z\"}}}],\"bikePath\":[{\"type\":\"FeatureCollection\",\"bbox\":[1.467649,43.584044,1.46773,43.584077],\"features\":[{\"bbox\":[1.467649,43.584044,1.46773,43.584077],\"type\":\"Feature\",\"properties\":{\"segments\":[{\"distance\":7.5,\"duration\":1.5,\"steps\":[{\"distance\":7.5,\"duration\":1.5,\"type\":11,\"instruction\":\"Head northwest on Rue Ferrando\",\"name\":\"Rue Ferrando\",\"way_points\":[0,1]},{\"distance\":0.0,\"duration\":0.0,\"type\":10,\"instruction\":\"Arrive at Rue Ferrando, on the right\",\"name\":\"-\",\"way_points\":[1,1]}]}],\"way_points\":[0,1],\"summary\":{\"distance\":7.5,\"duration\":1.5}},\"geometry\":{\"coordinates\":[[1.46773,43.584044],[1.467649,43.584077]],\"type\":\"LineString\"}}],\"metadata\":{\"attribution\":\"openrouteservice.org | OpenStreetMap contributors\",\"service\":\"routing\",\"timestamp\":1762429278556,\"query\":{\"coordinates\":[[1.4677301,43.5840438],[1.4677552,43.5842119]],\"profile\":\"cycling-regular\",\"profileName\":\"cycling-regular\",\"format\":\"geojson\"},\"engine\":{\"version\":\"9.3.0\",\"build_date\":\"2025-06-06T15:39:25Z\",\"graph_date\":\"2025-10-31T03:00:42Z\",\"osm_date\":\"2025-10-20T00:00:01Z\"}}}]}')
 
+        const pedestrian = response.pedestrianPath;
+        const bike = response.bikePath; // ← correction
 
-        const url = `http://localhost:8701/GPSServer/getItinerary?start=${departure}&end=${arrival}`;
-
-        console.log("Calling the API with the URL : \n", url)
-
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`Erreur HTTP: ${response.status}`);
-        }
-
-        const geojson = await response.json();
-
-        console.log("GeoJSON reçu :", geojson);
-
-
-
-        // Optionnel : tu pourras ensuite l’ajouter sur ta carte Leaflet
-        // L.geoJSON(geojson).addTo(map);
+        // Affichage des chemins
+        pedestrian.forEach(path => loadPath(path, "blue"));
+        bike.forEach(path => loadPath(path, "green"));
 
     } catch (err) {
         console.error("Erreur lors de la récupération de l’itinéraire :", err);
     }
 }
 
+function loadPath(data, color) {
+    const itineraire = L.geoJSON(data, {
+      style: {
+        color: color,
+        weight: 4
+      }
+    }).addTo(map);
 
-function loadPath() { }
+    map.fitBounds(itineraire.getBounds());
+}
 
 loadMap();
-
-// // Chargement du GeoJSON
-// fetch("itineraire.geojson")
-//   .then(response => response.json())
-//   .then(data => {
-//     const itineraire = L.geoJSON(data, {
-//       style: {
-//         color: 'red',
-//         weight: 4
-//       },
-//       onEachFeature: (feature, layer) => {
-//         if (feature.properties && feature.properties.name) {
-//           layer.bindPopup(feature.properties.name);
-//         }
-//       }
-//     }).addTo(map);
-
-//     // Zoom automatique sur l'itinéraire
-//     map.fitBounds(itineraire.getBounds());
-//   });
-
