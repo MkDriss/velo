f7ffa8661bb5b65e8bc7d096c396437eb5e2c90f
SuperDri <driss.meskini@outlook.fr>
2025-11-07 08:28:17 +0100

    self hosted

--- Velo/GPS_Server/Program.cs
+++ Velo/GPS_Server/Program.cs
@@ -2,6 +2,7 @@
 using System.ServiceModel;
 using System.ServiceModel.Description;
 using System.ServiceModel.Web;
+using Proxy; // important : référence au projet Proxy
 
 namespace GPS_Server
 {
@@ -9,19 +10,27 @@ namespace GPS_Server
     {
         static void Main(string[] args)
         {
-            var baseAddress = new Uri("http://localhost:8701/GPSServer/");
-            using (var host = new WebServiceHost(typeof(GPS), baseAddress))
+            // --- Démarrer ProxyCache via ton wrapper ---
+            Uri proxyAddress = new Uri("http://localhost:8733/proxyCache/");
+            ServiceHost proxyHost = ProxyHostFactory.Create(proxyAddress);
+            proxyHost.Open();
+            Console.WriteLine("ProxyCache running at " + proxyAddress);
+
+            // --- Démarrer GPSServer (inchangé) ---
+            var gpsAddress = new Uri("http://localhost:8701/GPSServer/");
+            using (var gpsHost = new WebServiceHost(typeof(GPS), gpsAddress))
             {
-                var endpoint = host.AddServiceEndpoint(typeof(IGPS), new WebHttpBinding(), "");
+                var endpoint = gpsHost.AddServiceEndpoint(typeof(IGPS), new WebHttpBinding(), "");
                 endpoint.Behaviors.Add(new WebHttpBehavior());
 
-
-                host.Open();
-                Console.WriteLine("GPSServer running at " + baseAddress);
-                Console.WriteLine("Press Enter to stop...");
+                gpsHost.Open();
+                Console.WriteLine("GPSServer running at " + gpsAddress);
+                Console.WriteLine("Press Enter to stop both services...");
                 Console.ReadLine();
             }
 
+            // -- Arrêter Proxy proprement --
+            proxyHost.Close();
         }
     }
 }
---
+++ Velo/Proxy/ProxyHostFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    public static class ProxyHostFactory
+    {
+        public static ServiceHost Create(Uri baseAddress)
+        {
+            return new ServiceHost(typeof(ProxyCache), baseAddress);
+        }
+    }
+}
